156c8f4097c0cc12153ed1dad72cda3c3e6698f2
Lucas Bustamante <lucascostabustamante@gmail.com>
2023-10-23 19:29:20 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <div>
             <a href="https://github.com/lucasbustamante/SPA_Senac_PTI" target="_blank">
-                <img src={('src/assets/logo.png')} className="logo foodlogo" alt="Logo" />
+                <img src={('./src/assets/logo.png')} className="logo foodlogo" alt="Logo" />
             </a>
         </div>
     )
