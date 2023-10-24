dfd1b8116186ec2e9b8810712df3cca15fc75d01
Lucas Bustamante <lucascostabustamante@gmail.com>
2023-10-23 21:30:22 -0300

    Update Titulo.jsx

--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -6,7 +6,8 @@ export default function Titulo() {
             <h1 className='titulo'>Bustamante's Food</h1>
             <h2 className='subtitulo'>
                 Este site foi concebido como parte de um projeto acadêmico da faculdade.<br />
-                O objetivo é criar uma plataforma fictícia para explorar e aplicar os conhecimentos adquiridos ao longo do curso,<br />
+                O objetivo é criar uma plataforma fictícia para explorar e 
+                aplicar os conhecimentos adquiridos ao longo do curso,<br />
                 demonstrando a capacidade de desenvolver um site funcional e atraente.
             </h2>
         </div>
